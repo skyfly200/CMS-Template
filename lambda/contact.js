dcdd5b6b15e3efd65ae13e1dd5a8f9495de73adf
Skyler Fly-Wilson <skyfly200@gmail.com>
2019-04-21 21:28:48 -0600

    fix query parsing

--- lambda/contact.js
+++ lambda/contact.js
@@ -56122,15 +56122,13 @@ exports.handler = function (event, context, callback) {
     return callback(null, generateResponse("Method Not Allowed", 405));
   } else if (!event.body) {
     // complain if event body is empty
-    console.log("1");
     return callback(null, generateResponse("Invalid Request", 204));
   } //-- Make sure we have all required data. Otherwise, complain.
 
 
-  const data = querystring__WEBPACK_IMPORTED_MODULE_0___default.a.parse(event.body);
+  const data = JSON.parse(event.body);
 
   if (!data.name || !data.email || !data.message) {
-    console.log("2", data);
     return callback(null, generateResponse("Missing Information", 204));
   } // build the email object from the request body
 
@@ -56145,14 +56143,12 @@ exports.handler = function (event, context, callback) {
 
   try {
     mailgun.messages().send(email, (error, body) => {
-      console.log("3");
       let resp = generateResponse({
         body
       }, 200);
       return callback(null, resp);
     });
   } catch (error) {
-    console.log("4");
     let resp = generateResponse("Server Error", 500);
     console.error(error);
     return callback(error);
--- src/contact.js
+++ src/contact.js
@@ -16,17 +16,15 @@ exports.handler = function(event, context, callback) {
   if (event.httpMethod !== "POST") { // Only run on POST requests
     return callback(null, generateResponse("Method Not Allowed", 405));
   } else if (!event.body) { // complain if event body is empty
-    console.log("1");
     return callback(null, generateResponse("Invalid Request", 204));
   }
   //-- Make sure we have all required data. Otherwise, complain.
-  const data = querystring.parse(event.body);
+  const data = JSON.parse(event.body);
   if (
     !data.name ||
     !data.email ||
     !data.message
   ) {
-    console.log("2", data);
     return callback(null, generateResponse("Missing Information", 204));
   }
   // build the email object from the request body
@@ -40,12 +38,10 @@ exports.handler = function(event, context, callback) {
   // attempt to send email
   try {
     mailgun.messages().send(email, (error, body) => {
-      console.log("3");
       let resp = generateResponse({body}, 200);
       return callback(null, resp);
     });
   } catch (error) {
-    console.log("4");
     let resp = generateResponse("Server Error", 500);
     console.error(error);
     return callback(error);
